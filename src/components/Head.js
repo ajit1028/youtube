ea401eb9fc4d4700a203cb0a8411226b0c99133c
ajit <ajeets@1028.com>
2024-08-16 22:25:24 +0530

    youtube-body-sidebar-head-watch

---
+++ src/components/Head.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-5  m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-9 cursor-pointer"
+          alt="menu"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0evWy6vmua96UkF8RqHQv-SoBcuu3V9fwZw&s"
+        />
+        <a href="/">
+          <img
+            className="h-10 mx-2 "
+            alt="youtube-logo"
+            src="https://cdn.icon-icons.com/icons2/2699/PNG/512/youtube_logo_icon_167938.png"
+          />
+        </a>
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
